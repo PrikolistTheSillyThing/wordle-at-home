5c67a14e20024efad3e26bf8117a2d5dc9296f0b
PrikolistTheSillyThing <uzun.andrei13@gmail.com>
2026-07-27 16:35:18 +0300

    very VERY basic random word picker implemented

--- src/dev/thingy/Main.java
+++ src/dev/thingy/Main.java
@@ -1,4 +1,31 @@
 package dev.thingy;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+
 public class Main {
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public static void playTheGame() {
+        String word = wordPicker();
+    }
+
+    public static String wordPicker() {
+        List<String> array = new ArrayList<>(List.of(
+                "DRIVE", "LADDER", "FATTER", "MARGIN" //Obviously will be changed
+        ));
+        Random random = new Random();
+        int rand = random.nextInt(array.size());
+
+        return array.get(rand);
+    }
 }
